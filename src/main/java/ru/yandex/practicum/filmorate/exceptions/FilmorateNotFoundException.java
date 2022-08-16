51d3a6e45a9c43206ad31a32b270b859208f9518
Bervy <vladshelkovo@yandex.ru>
2022-08-16 14:03:27 +0300

    create - создал 2 обших исключения

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmorateNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class FilmorateNotFoundException extends RuntimeException {
+
+    public FilmorateNotFoundException(String message) {
+        super(message);
+    }
+}
