654eea334f329c6773ffa6fe24084b1da03c6ae3
Bervy <vladshelkovo@yandex.ru>
2022-08-16 14:06:11 +0300

    fix - none

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.service.user.UserService;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -56,28 +56,28 @@ public class UserController extends AbstractController<User> {
 
 
     @PutMapping("/{id}/friends/{friendId}")
-    public void addFriend(@PathVariable("id") @Min(0)long userId,
-                          @PathVariable("friendId") @Min(0)long friendId) {
+    public void addFriend(@PathVariable("id") @Min(0) long userId,
+                          @PathVariable("friendId") @Min(0) long friendId) {
         log.info("Request received for add friend to user with id {}", userId);
         userService.addFriend(userId, friendId);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public void deleteFriend(@PathVariable("id") @Min(0)long userId,
-                             @PathVariable("friendId") @Min(0)long friendId) {
+    public void deleteFriend(@PathVariable("id") @Min(0) long userId,
+                             @PathVariable("friendId") @Min(0) long friendId) {
         log.info("Request received for delete friend from user with id {}", userId);
         userService.deleteFriend(userId, friendId);
     }
 
     @GetMapping("/{id}/friends")
-    public List<User> getFriends(@PathVariable("id") @Min(0)long userId) {
+    public List<User> getFriends(@PathVariable("id") @Min(0) long userId) {
         log.info("Request received for get friends from user with id {}", userId);
         return userService.getFriends(userId);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable("id") @Min(0)long userId,
-                                       @PathVariable("otherId") @Min(0)long friendId) {
+    public List<User> getCommonFriends(@PathVariable("id") @Min(0) long userId,
+                                       @PathVariable("otherId") @Min(0) long friendId) {
         log.info("Request received for get common friends  user with id {} " +
                 "and another user with id {}", userId, friendId);
         return userService.getCommonFriends(userId, friendId);
