5295a4b35053015c6b0545987de1f03d265dafe2
Bervy <vladshelkovo@yandex.ru>
2022-08-16 14:01:05 +0300

    delete - Удалил лишние исключения

--- src/main/java/ru/yandex/practicum/filmorate/error/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/error/ErrorHandler.java
@@ -6,36 +6,30 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exceptions.*;
+import ru.yandex.practicum.filmorate.exceptions.FilmorateAlreadyExistsException;
+import ru.yandex.practicum.filmorate.exceptions.FilmorateNotFoundException;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler({FilmNotFoundException.class, UserNotFoundException.class,
-            LikeNotFoundException.class})
+    @ExceptionHandler(FilmorateNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final RuntimeException e) {
-        return new ErrorResponse("Not found",e.getMessage());
+        return new ErrorResponse("Not found", e.getMessage());
     }
 
-    @ExceptionHandler(CountFilmException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleWrongCountFilmsException(final CountFilmException e) {
-        return new ErrorResponse("Bad request", e.getMessage());
+    @ExceptionHandler(FilmorateAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleAlreadyExistsException(final RuntimeException e) {
+        return new ErrorResponse("Conflict", e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleWrongCountFilmsException(final ValidationException e) {
+    public ErrorResponse handleWrongCountFilmsException(final MethodArgumentNotValidException e) {
         return new ErrorResponse("Bad request", e.getMessage());
-    }
-
-    @ExceptionHandler({FilmAlreadyExistsException.class, UserAlreadyExistsException.class})
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse handleAlreadyExistsException(final RuntimeException e) {
-        return new ErrorResponse("Conflict", e.getMessage());
     }
 
     @ExceptionHandler
